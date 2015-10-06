1fa13fd80a0dbc3c7781d6704e4a940c688a91fe
iso- <ismk60@gmail.com>
2015-10-06 18:08:01 +0200

    Ajout scripts des listes chainees

--- pretraitement2.h
+++ pretraitement2.h
@@ -3,6 +3,6 @@
 Uint32 getpixel(SDL_Surface *surface,unsigned x,unsigned y);
 void defPixel(SDL_Surface *surface,int x,int y,Uint32 color);
 void goToGrey(SDL_Surface *surface);
-void browse(SDL_Surface *image);
+void browse(SDL_Surface *image, int size);
 void Integral(SDL_Surface *image);
 #endif
